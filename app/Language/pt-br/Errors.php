ae0c47ba62773e0e60098c79ca465967467a4f5e
Marcos Ferreira <marcosferreira.developer@gmail.com>
2025-06-19 09:49:41 -0300

    altera locale padrão para pt-br e adiciona arquivos de tradução de erros e validação

---
+++ app/Language/pt-br/Errors.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'whoops' => 'Ops!',
+    'weHitASnag' => 'Encontramos um problema.',
+    'pageNotFound' => 'Página não encontrada',
+    'sorryCannotFind' => 'Desculpe, não conseguimos encontrar a página que você está procurando.',
+    'badRequest' => 'Solicitação inválida',
+    'sorryBadRequest' => 'Desculpe, sua solicitação não pôde ser processada.',
+];
