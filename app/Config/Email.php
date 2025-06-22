2f4a06752b223c2b9f1f822ab92e70e2285f6776
Marcos Ferreira <marcosferreira.developer@gmail.com>
2025-06-22 20:10:11 -0300

    adiciona configuração de autenticação e autorização com CodeIgniter Shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'suporte@pmdonaines.pb.gov.br';
+    public string $fromName   = 'Suporte de TI';
     public string $recipients = '';
 
     /**
