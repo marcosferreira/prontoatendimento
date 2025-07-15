950a319822da1161190eeea973f83604915e5c42
Marcos Ferreira <marcosferreira.developer@gmail.com>
2025-07-15 08:52:28 -0300

    feat(user-info): adicionar dropdown para informações do usuário com estilo personalizado

--- app/Views/components/user_info.php
+++ app/Views/components/user_info.php
@@ -1,28 +1,39 @@
-<div class="user-info">
-    <div class="user-avatar">DR</div>
-    <div class="user-details">
-        <?php
-        $user = auth()->user();
-        $userGroups = $user ? $user->getGroups() : [];
-        $groupNames = [];
+<div class="user-profile-container dropdown">
+    <div class="user-info" id="userProfileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <div class="user-avatar">
+            <?= strtoupper(substr(auth()->user()->username, 0, 2)) ?>
+        </div>
+        <div class="user-details">
+            <?php
+            $user = auth()->user();
+            $userGroups = $user ? $user->getGroups() : [];
+            $groupNames = [];
 
-        if (!empty($userGroups)) {
-            // Obter a configuração dos grupos
-            $authGroups = config('AuthGroups');
-            $availableGroups = $authGroups->groups ?? [];
+            if (!empty($userGroups)) {
+                // Obter a configuração dos grupos
+                $authGroups = config('AuthGroups');
+                $availableGroups = $authGroups->groups ?? [];
 
-            foreach ($userGroups as $group) {
-                if (isset($availableGroups[$group]['title'])) {
-                    $groupNames[] = $availableGroups[$group]['title'];
-                } else {
-                    $groupNames[] = ucfirst($group);
+                foreach ($userGroups as $group) {
+                    if (isset($availableGroups[$group]['title'])) {
+                        $groupNames[] = $availableGroups[$group]['title'];
+                    } else {
+                        $groupNames[] = ucfirst($group);
+                    }
                 }
             }
-        }
 
-        $displayRole = !empty($groupNames) ? implode(', ', $groupNames) : 'Usuário';
-        ?>
-        <p class="user-name"><?php echo $user->username ?? 'Usuário' ?></p>
-        <p class="user-role"><?php echo $displayRole ?></p>
+            $displayRole = !empty($groupNames) ? implode(', ', $groupNames) : 'Usuário';
+            ?>
+            <p class="user-name"><?php echo $user->username ?? 'Usuário' ?></p>
+            <p class="user-role"><?php echo $displayRole ?></p>
+        </div>
+        <i class="bi bi-chevron-down dropdown-arrow"></i>
+    </div>
+    <div class="dropdown-menu user-dropdown" aria-labelledby="userProfileDropdown">
+        <a class="dropdown-item" href="<?= site_url('logout') ?>">
+            <i class="bi bi-box-arrow-right"></i>
+            <span>Sair</span>
+        </a>
     </div>
 </div>
